c9341039e45291e42a8e44cde1a11f70ec40c28d
Aldo Paz Velasquez <aldveq93@Aldos-Mac-mini.local>
2024-03-09 21:42:47 -0600

    cleaning up unnecessary assets for the frontend

--- inc/Classes/APVPortfolioInit.php
+++ inc/Classes/APVPortfolioInit.php
@@ -17,6 +17,27 @@ if (!class_exists('APVPortfolioInit')) :
 			add_action('after_setup_theme', array($this, 'apv_portfolio_content_width'), 0);
 			add_action('widgets_init', array($this, 'apv_portfolio_widgets_init'));
 			add_action('wp_enqueue_scripts', array($this, 'apv_portfolio_scripts'));
+			
+			// Remove emoji support
+			remove_action('wp_head', 'print_emoji_detection_script', 7);
+			remove_action('wp_print_styles', 'print_emoji_styles');
+
+			// Remove rss feed links
+			remove_action( 'wp_head', 'feed_links_extra', 3 );
+			remove_action( 'wp_head', 'feed_links', 2 );
+
+			// // remove wp-embed
+			add_action( 'wp_footer', function () {
+				wp_dequeue_script( 'wp-embed' );
+			});
+
+			add_action( 'wp_enqueue_scripts', function() {
+				// // remove block library css
+				wp_dequeue_style( 'wp-block-library' );
+
+				// // remove comment reply JS
+				wp_dequeue_script( 'comment-reply' );
+			} );
 		}
 
 		public function define_constants()
