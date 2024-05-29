26f414d34b3e1157a0aebfc88874609704c09695
Aldo Paz Velasquez <aldveq93@Aldos-Mac-mini.local>
2024-05-28 20:57:20 -0600

    fix | added project cat link to project grid item

--- template-parts/cpt-projects/project-grid-item.php
+++ template-parts/cpt-projects/project-grid-item.php
@@ -11,25 +11,45 @@ $project_term_by_post = get_the_terms( get_the_ID(), 'projects-category' );
 ?>
 	<div class="mil-grid-item">
 
-		<a href="<?php echo esc_url( get_the_permalink( get_the_ID() ) ); ?>" class="mil-portfolio-item mil-square-item mil-mb-60" target="_self">
+		<div class="mil-portfolio-item mil-square-item mil-mb-60">
 			<div class="mil-cover">
-				<?php
-					echo wp_get_attachment_image(
-						get_post_thumbnail_id( get_the_ID() ),
-						'project-featured-image',
-						false,
-						array( 'load' => 'lazy' )
-					);
+				<a 
+					href="<?php echo esc_url( get_the_permalink( get_the_ID() ) ); ?>" 
+					class="img-permalink"
+					target="_self"
+				>
+					<?php
+						echo wp_get_attachment_image(
+							get_post_thumbnail_id( get_the_ID() ),
+							'project-featured-image',
+							false,
+							array( 'load' => 'lazy' )
+						);
 					?>
-				<div class="mil-hover-link">
-					<i class="fas fa-link"></i>
-				</div>
+					<div class="mil-hover-link">
+						<i class="fas fa-link"></i>
+					</div>
+				</a>
 			</div>
 			<div class="mil-project-descr">
-				<p class="mil-upper mil-accent mil-mb-15"><?php echo esc_html( $project_term_by_post[0]->name ); ?></p>
-				<h4 class="mil-up"><?php echo esc_html( the_title() ); ?></h4>
+				<p class="mil-upper mil-accent mil-mb-15">
+					<a 
+						href="<?php echo esc_url( get_term_link( $project_term_by_post[0]->term_id ) ); ?>"
+						target="_self"
+					>
+						<?php echo esc_html( $project_term_by_post[0]->name ); ?>
+					</a>
+				</p>
+				<h4 class="mil-up">
+					<a 
+						href="<?php echo esc_url( get_the_permalink( get_the_ID() ) ); ?>"
+						target="_self"
+					>
+						<?php echo esc_html( the_title() ); ?>
+					</a>
+				</h4>
 			</div>
-		</a>
+		</div>
 
 	</div>
 <?php
